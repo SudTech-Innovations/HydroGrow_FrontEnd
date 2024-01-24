aeff881de45a570360ca01cf65b9fa2855cfcea2
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-01-24 13:15:27 +0100

    Create Footer.jsx

---
+++ hydrogrow/src/pages/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Copyright from "../../components/Footer/copyright";
+
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div id="footer" className="cl-bar">
+      <Copyright />
+    </div>
+  );
+}
