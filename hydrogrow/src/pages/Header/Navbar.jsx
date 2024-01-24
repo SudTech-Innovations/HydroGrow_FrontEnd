1ed415e04700f1b62479eb833ee5d5a8d0986581
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-01-24 14:07:25 +0100

    Update Navbar.jsx

--- hydrogrow/src/pages/Header/Navbar.jsx
+++ hydrogrow/src/pages/Header/Navbar.jsx
@@ -5,9 +5,12 @@ import "./Navbar.css";
 export default function Navbar() {
   return (
     <div id="navbar" className="cl-bar">
+      {/* Navbar du coté gauche */}
       <div className="navbar-left">
         <a href="/">HydroGrow</a>
       </div>
+
+      {/* Navbar du coté droit */}
       <div className="navbar-right">
         <a href="/">Accueil</a>
         <a href="/about">A propos</a>
