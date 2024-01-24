ad1542e8f583a260a5d097c78de5c52df1774b3e
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-01-24 13:15:25 +0100

    Create Navbar.jsx

---
+++ hydrogrow/src/pages/Header/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div id="navbar" className="cl-bar">
+      <a href="/">Accueil</a>
+      <a href="/about">A propos</a>
+    </div>
+  );
+}
