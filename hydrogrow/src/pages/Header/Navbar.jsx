281a81302731f2c46e7bbc8a0a06c6b2e7ce2ed6
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-01-24 13:37:49 +0100

    Navbar

--- hydrogrow/src/pages/Header/Navbar.jsx
+++ hydrogrow/src/pages/Header/Navbar.jsx
@@ -5,8 +5,13 @@ import "./Navbar.css";
 export default function Navbar() {
   return (
     <div id="navbar" className="cl-bar">
-      <a href="/">Accueil</a>
-      <a href="/about">A propos</a>
+      <div className="navbar-left">
+        <a href="/">HydroGrow</a>
+      </div>
+      <div className="navbar-right">
+        <a href="/">Accueil</a>
+        <a href="/about">A propos</a>
+      </div>
     </div>
   );
 }
