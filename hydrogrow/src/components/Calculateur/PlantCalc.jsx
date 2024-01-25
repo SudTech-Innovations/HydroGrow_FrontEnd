153320377de8c0adc80b4e581b6ed6f892661950
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-01-25 15:53:44 +0100

    Update PlantCalc.jsx

--- hydrogrow/src/components/Calculateur/PlantCalc.jsx
+++ hydrogrow/src/components/Calculateur/PlantCalc.jsx
@@ -1,20 +1,13 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
 export default function PlantCalc({
   concentrationEng,
-  handleConcentrationEngChange,
   needsPlant,
-  handleNeedsPlantChange,
   quantityEng,
-  resetValues,
 }) {
   useEffect(() => {
-    // Calculate quantityEng every time any of the input values change
     if (concentrationEng > 0 && needsPlant > 0) {
-      // Note: Avoid setting state directly, use the prop function
       quantityEng(concentrationEng / needsPlant);
     }
-  }, [concentrationEng, needsPlant]);
-
-  return null; // Replace with your actual UI
+  }, [concentrationEng, needsPlant, quantityEng]);
 }
