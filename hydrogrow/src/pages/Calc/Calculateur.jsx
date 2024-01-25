86a5286574c27ea79db7ae5e8600fbe465626a8c
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-01-25 14:08:48 +0100

    rename folder

---
+++ hydrogrow/src/pages/Calc/Calculateur.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+import Quantite from "./Modules/Quantite";
+
+import "./Calculateur.css";
+
+export default function Calculateur() {
+  return (
+    <div id="calculateur">
+      <Quantite />
+    </div>
+  );
+}
