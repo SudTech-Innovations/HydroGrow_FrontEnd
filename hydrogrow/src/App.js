98997ac36abb2b99f1414afe475cf697894b752e
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-01-25 14:08:22 +0100

    for rename

--- hydrogrow/src/App.js
+++ hydrogrow/src/App.js
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./pages/Header/Navbar";
 import Footer from "./pages/Footer/Footer";
 import Home from "./pages/Home/Home";
-import Calculateur from "./pages/About/Calculateur";
+import Calculateur from "./pages/Calc/Calculateur";
 import Error from "./pages/Error/Error";
 
 import "./App.css";
